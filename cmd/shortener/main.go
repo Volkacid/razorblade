50bc257c66a226280ac35fef012704a8d8bc20b8
VolkAcid <moshenokdenis@mail.ru>
2023-01-16 11:44:38 +0300

    gRPC service implementation

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/Volkacid/razorblade/internal/app/config"
+	pb "github.com/Volkacid/razorblade/internal/app/grpc/proto"
+	server2 "github.com/Volkacid/razorblade/internal/app/grpc/server"
 	"github.com/Volkacid/razorblade/internal/app/server"
 	"github.com/Volkacid/razorblade/internal/app/server/middlewares"
+	"github.com/Volkacid/razorblade/internal/app/service"
 	"github.com/Volkacid/razorblade/internal/app/storage"
 	"github.com/go-chi/chi/v5"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 	"net/http"
 )
 
@@ -16,6 +22,9 @@ func main() {
 	defer cancel()
 	servConf := config.GetServerConfig()
 	db := storage.CreateStorage()
+
+	go gRPCService(db, ctx)
+
 	handlers := server.NewHandlersSet(db, ctx)
 
 	router := chi.NewRouter()
@@ -32,4 +41,17 @@ func main() {
 		router.Delete("/api/user/urls", handlers.DeleteUserURLs)
 	})
 	log.Fatal(http.ListenAndServe(servConf.ServerAddress, router))
+}
+
+func gRPCService(db storage.Storage, ctx context.Context) {
+	gRPCListener, err := net.Listen("tcp", ":3200")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rpcServer := grpc.NewServer()
+	pb.RegisterUsersServer(rpcServer, &server2.RazorbladeService{DB: db, DeleteBuffer: service.NewDeleteBuffer(db, ctx)})
+	fmt.Println("Starting a gRPC server")
+	if err = rpcServer.Serve(gRPCListener); err != nil {
+		log.Fatal(err)
+	}
 }
